43672b41fa7af517146fe4350003288b3e66f16c
Franziska Chen <chenfranziska@gmail.com>
2024-05-21 20:04:53 +0200

    Put everything into App.js

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ export default function App() {
     setSelectedImage(image);
     setImageSelf(image.blank);
   }
+
+  // Create a variable to remove the ".png" from the end of the URL
   const memeSelf = imageSelf.replace(/.png/g, '');
 
   // Create function that sets the selectedImage state to null, effectively removing the selected image display
@@ -45,82 +47,98 @@ export default function App() {
     <main className={styles.row}>
       <section className={styles.column}>
         <h1>React Meme Generator</h1>
-
-        {/* Create a user guide on how to use the meme generator  */}
-        <div className={styles.column}>
-          {' '}
-          <span>
-            <p style={{ fontWeight: 'bold' }}>Create Your Own Meme:</p>
-            <ol>
-              <li>
-                <span style={{ fontWeight: 'bold' }}>Select an image: </span>
-                Browse or click an image on the right.
-              </li>
-              <li>
-                <span style={{ fontWeight: 'bold' }}>Add text: </span>Type in
-                the Top or Bottom text fields to add text to the image.
-                <br />
-                <span style={{ fontSize: 'small' }}>
-                  Use letters and numbers only. No special characters.
-                </span>
-              </li>
-              <li>
-                <span style={{ fontWeight: 'bold' }}>Download: </span>Click the
-                download button to save your meme.
-              </li>
-            </ol>
-            <p>Enjoy creating!</p>
-          </span>
-          {/* Create two input fields for the top and bottom text */}
-          <form onSubmit={(event) => event.preventDefault()}>
-            <div>
+        <div className={styles.row}>
+          {/* Create a user guide on how to use the meme generator  */}
+          <div className={styles.column}>
+            {' '}
+            <span>
+              <p style={{ fontWeight: 'bold' }}>Create Your Own Meme:</p>
+              <ol>
+                <li>
+                  <span style={{ fontWeight: 'bold' }}>Select an image: </span>
+                  Browse or click an image on the right.
+                </li>
+                <li>
+                  <span style={{ fontWeight: 'bold' }}>Add text: </span>Type in
+                  the Top or Bottom text fields to add text to the image.
+                  <br />
+                  <span style={{ fontSize: 'small' }}>
+                    Use letters and numbers only. No special characters.
+                  </span>
+                </li>
+                <li>
+                  <span style={{ fontWeight: 'bold' }}>Download: </span>Click
+                  the download button to save your meme.
+                </li>
+              </ol>
+              <p>Enjoy creating!</p>
+            </span>
+            {/* Create two input fields for the top and bottom text */}
+            <form onSubmit={(event) => event.preventDefault()}>
               <div>
-                <span style={{ fontWeight: 'bold' }}>Top Text: </span>
-                <p>{topText}</p>
-                <br />
-                <input
-                  placeholder="Type your top text"
-                  value={topText}
-                  onChange={(event) => setTopText(event.currentTarget.value)}
-                />
-                <br />
-                <br />
-                <span style={{ fontWeight: 'bold' }}>Bottom Text: </span>
-                <p>{bottomText}</p>
-                <br />
-                <input
-                  placeholder="Type your bottom text"
-                  value={bottomText}
-                  onChange={(event) => setBottomText(event.currentTarget.value)}
-                />
+                <div>
+                  <span style={{ fontWeight: 'bold' }}>Top Text: </span>
+                  {topText}
+                  <br />
+                  <input
+                    label="Top text"
+                    placeholder="Type your top text"
+                    value={topText}
+                    onChange={(event) => setTopText(event.currentTarget.value)}
+                  />
+                  <br />
+                  <br />
+                  <span style={{ fontWeight: 'bold' }}>Bottom Text: </span>
+                  {bottomText}
+                  <br />
+                  <input
+                    label="Bottom text"
+                    placeholder="Type your bottom text"
+                    value={bottomText}
+                    onChange={(event) =>
+                      setBottomText(event.currentTarget.value)
+                    }
+                  />
+                </div>
               </div>
-            </div>
-          </form>
-        </div>
-
-        {/* Create a preview of the generated meme */}
-        <div className={styles.column}>
-          {' '}
-          <div className={styles.imgPreview}>
-            <img
-              src={`${memeSelf}/${topText}_/${bottomText}.png`}
-              alt={imageSelf.name}
-              style={{ width: '300px' }}
-            />
-          </div>
-          {/* Create a download button  */}
-          <div>
-            <form
-              method="get"
-              action={`${memeSelf}/${topText}_/${bottomText}.png`}
-            >
-              <button>
-                <span>
-                  <FaDownload />
-                </span>
-                <span>&nbsp;&nbsp;Download your meme</span>
-              </button>
             </form>
+          </div>
+
+          {/* Create a preview of the generated meme */}
+          <div className={styles.column}>
+            <div className={styles.imgPreview}>
+              {memeSelf ? (
+                <img
+                  src={`${memeSelf}/${topText}_/${bottomText}.png`}
+                  alt={imageSelf.name}
+                  style={{ width: '300px' }}
+                  data-test-id="meme-image"
+                />
+              ) : (
+                <div>
+                  <img
+                    src="https://api.memegen.link/images/buzz/your-top-text/your-bottom-text.png"
+                    alt="buzz"
+                    style={{ width: '100%' }}
+                  />
+                  <center>EXAMPLE ONLY 😌</center>
+                </div>
+              )}
+            </div>
+            {/* Create a download button  */}
+            <div>
+              <form
+                method="get"
+                action={`${memeSelf}/${topText}_/${bottomText}.png`}
+              >
+                <button>
+                  <span>
+                    <FaDownload />
+                  </span>
+                  <span>&nbsp;&nbsp;Download</span>
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </section>
@@ -137,7 +155,7 @@ export default function App() {
           </div>
 
           {/* If selectedImage is NOT null, a box is displayed above the image gallery with the selected image and its title */}
-          <div>
+          <div label="Meme template">
             {selectedImage && (
               <div className={styles.selectedImageBoxStyles}>
                 <span

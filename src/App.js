887da4f97e4c82da008ad01806a351a94a4d1eff
Franziska Chen <chenfranziska@gmail.com>
2024-05-24 09:00:18 +0200

    Add submit button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ export default function SimpleSolution() {
   const [topText, setTopText] = useState('hi');
   const [bottomText, setBottomText] = useState('');
   const [memeName, setMemeName] = useState('bender');
+  const [image, setImage] = useState(
+    `https://api.memegen.link/images/${memeName}/${topText}/${bottomText}.png`,
+  );
 
   const url = `https://api.memegen.link/images/${memeName}/${topText}/${bottomText}.png`;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setImage(url);
+  }
 
   function handleDownload(event) {
     event.preventDefault();
@@ -58,11 +66,7 @@ export default function SimpleSolution() {
         />
 
         <label htmlFor="Meme template">Meme template</label>
-        <img
-          src={`https://api.memegen.link/images/${memeName}/${topText}/${bottomText}.png`}
-          alt="Meme template"
-          data-test-id="meme-image"
-        />
+        <img src={image} alt="Meme template" data-test-id="meme-image" />
         <label htmlFor="Meme Name">Meme Name</label>
         <input
           name="Meme Name"
@@ -70,9 +74,9 @@ export default function SimpleSolution() {
           label="Meme Name"
           placeholder="Search for a meme"
           value={memeName}
-          onClick={() => setMemeName('')}
           onChange={(event) => setMemeName(event.currentTarget.value)}
         />
+        <button onClick={handleSubmit}>Submit</button>
       </form>
 
       <form>
